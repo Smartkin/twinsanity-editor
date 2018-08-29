7560b2175d31134514f740d6207de2444b11ce2b
ManDude <mandude@users.noreply.github.com>
2018-08-29 20:09:56 +0100

    test commit
    agagagagaga

---
+++ Twinsanity/Types.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twinsanity
+{
+    public struct Vector4
+    {
+        private float x, y, z, w;
+
+        public Vector4(float x, float y, float z, float w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public float W { get; set; }
+    }
+}
