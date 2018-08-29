357f2379ed80e68a5fad9de3b7cae95ca06e3a2d
ManDude <mandude@users.noreply.github.com>
2018-08-29 20:26:09 +0100

    Fixing Types.cs

--- Twinsanity/Types.cs
+++ Twinsanity/Types.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Twinsanity
+﻿namespace Twinsanity
 {
     public struct Vector4
     {
         private float x, y, z, w;
 
-        public Vector4(float x, float y, float z, float w)
+        public Vector4(float x, float y, float z, float w) : this()
         {
             this.x = x;
             this.y = y;
