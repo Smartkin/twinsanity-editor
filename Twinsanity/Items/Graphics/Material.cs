12292d2e9ddc0224229918f0ab56c915b8556659
ManDude <mandude@users.noreply.github.com>
2018-09-12 02:54:24 +0100

    Fix Material format

--- Twinsanity/Items/Graphics/Material.cs
+++ Twinsanity/Items/Graphics/Material.cs
@@ -4,16 +4,16 @@ namespace Twinsanity
 {
     public class Material : TwinsItem
     {
-        private string Name { get; set; }
-        private int Header { get; set; }
-        private int Unknown { get; set; }
-        private int Type { get; set; }
-        private uint Tex { get; set; }
-        private uint Last { get; set; }
-        private byte[] UnkArray1 { get; set; }
-        private byte[] UnkArray2 { get; set; }
-        private float[] ValuesF { get; set; } = new float[6];
-        private int[] ValuesI { get; set; } = new int[2];
+        public string Name { get; set; }
+        public int Header { get; set; }
+        public int Unknown { get; set; }
+        public int Type { get; set; }
+        public uint Tex { get; set; }
+        public uint Last { get; set; }
+        public byte[] UnkArray1 { get; set; }
+        public byte[] UnkArray2 { get; set; }
+        public float[] ValuesF { get; set; } = new float[4];
+        public int[] ValuesI { get; set; } = new int[4];
 
 
         public override void Save(BinaryWriter writer)
@@ -24,15 +24,15 @@ namespace Twinsanity
             writer.Write(Name.Length + 1);
             writer.Write(Name);
             writer.Write(UnkArray1);
+            writer.Write(ValuesI[0]);
+            writer.Write(ValuesI[1]);
+            writer.Write(ValuesI[2]);
+            writer.Write(ValuesI[3]);
+            writer.Write(UnkArray2);
             writer.Write(ValuesF[0]);
             writer.Write(ValuesF[1]);
             writer.Write(ValuesF[2]);
             writer.Write(ValuesF[3]);
-            writer.Write(UnkArray2);
-            writer.Write(ValuesF[4]);
-            writer.Write(ValuesF[0]);
-            writer.Write(ValuesF[5]);
-            writer.Write(ValuesF[1]);
             writer.Write(Tex);
             writer.Write(Last);
         }
@@ -47,16 +47,16 @@ namespace Twinsanity
             reader.BaseStream.Position++;
             UnkArray1 = new byte[0x2A];
             UnkArray1 = reader.ReadBytes(0x2A);
+            ValuesI[0] = reader.ReadInt32();
+            ValuesI[1] = reader.ReadInt32();
+            ValuesI[2] = reader.ReadInt32();
+            ValuesI[3] = reader.ReadInt32();
+            UnkArray2 = new byte[0x10];
+            UnkArray2 = reader.ReadBytes(0x10);
             ValuesF[0] = reader.ReadSingle();
             ValuesF[1] = reader.ReadSingle();
             ValuesF[2] = reader.ReadSingle();
             ValuesF[3] = reader.ReadSingle();
-            UnkArray2 = new byte[0x10];
-            UnkArray2 = reader.ReadBytes(0x10);
-            ValuesF[4] = reader.ReadSingle();
-            ValuesF[0] = reader.ReadInt32();
-            ValuesF[5] = reader.ReadSingle();
-            ValuesF[1] = reader.ReadInt32();
             Tex = reader.ReadUInt32();
             Last = reader.ReadUInt32();
         }
