7f26cf8d41051c32e068e4115756e8ce1babf355
ManDude <mandude@users.noreply.github.com>
2018-09-16 03:12:23 +0100

    Show triggers in RMViewer (T to toggle, C toggles collision nodes now)

--- TwinsaityEditor/Viewers/RMViewer.cs
+++ TwinsaityEditor/Viewers/RMViewer.cs
@@ -1,5 +1,6 @@
 ﻿using OpenTK;
 using OpenTK.Graphics.OpenGL;
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 using Twinsanity;
@@ -11,7 +12,7 @@ namespace TwinsaityEditor
         private int dlist_col = -1, dlist_trg = -1;
         private ColData data;
         private Color[] colors = new[] { Color.Gray, Color.Green, Color.Red, Color.DarkBlue, Color.Yellow, Color.Pink, Color.DarkCyan, Color.DarkGreen, Color.DarkRed, Color.Brown, Color.DarkMagenta, Color.Orange, Color.DarkSeaGreen, Color.Bisque, Color.Coral };
-        private bool show_trigger;
+        private bool show_col_nodes, show_triggers;
         private TwinsFile file;
 
         private readonly float indicator_size = 0.5f;
@@ -20,7 +21,7 @@ namespace TwinsaityEditor
         {
             //initialize variables here
             dlist_col = dlist_trg = -1;
-            show_trigger = false;
+            show_col_nodes = show_triggers = false;
             this.data = data;
             this.file = file;
         }
@@ -50,7 +51,7 @@ namespace TwinsaityEditor
             }
             else
                 GL.CallList(dlist_col);
-            if (show_trigger)
+            if (show_col_nodes)
             {
                 if (dlist_trg == -1)
                 {
@@ -91,6 +92,7 @@ namespace TwinsaityEditor
                     GL.CallList(dlist_trg);
             }
 
+            //Draw instances (solid surfaces)
             for (uint i = 0; i <= 7; ++i)
             {
                 if (file.SecInfo.Records.ContainsKey(i))
@@ -104,18 +106,7 @@ namespace TwinsaityEditor
                             GL.Rotate(-j.RotX / (float)ushort.MaxValue * 360f, 1, 0, 0);
                             GL.Rotate(-j.RotY / (float)ushort.MaxValue * 360f, 0, 1, 0);
                             GL.Rotate(-j.RotZ / (float)ushort.MaxValue * 360f, 0, 0, 1);
-                            GL.Begin(PrimitiveType.Lines);
-                            GL.Color3(1f, 0f, 0f);
-                            float new_ind_size = indicator_size / 2;
-                            GL.Vertex3(+new_ind_size, 0, 0);
-                            GL.Vertex3(-new_ind_size, 0, 0);
-                            GL.Color3(0f, 1f, 0f);
-                            GL.Vertex3(0, +new_ind_size, 0);
-                            GL.Vertex3(0, -new_ind_size, 0);
-                            GL.Color3(0f, 0f, 1f);
-                            GL.Vertex3(0, 0, +new_ind_size);
-                            GL.Vertex3(0, 0, -new_ind_size);
-                            GL.End();
+                            DrawAxes(0, 0, 0, 0.5f);
                             GL.Color3(colors[colors.Length - i - 1]);
                             GL.Begin(PrimitiveType.LineStrip);
                             GL.Vertex3(-indicator_size, -indicator_size + 0.5, -indicator_size);
@@ -144,6 +135,118 @@ namespace TwinsaityEditor
                     }
                 }
             }
+            //Draw triggers (transparent surfaces)
+            for (uint i = 0; i <= 7; ++i)
+            {
+                if (file.SecInfo.Records.ContainsKey(i))
+                {
+                    if (((TwinsSection)file.SecInfo.Records[i]).SecInfo.Records.ContainsKey(7) && show_triggers)
+                    {
+                        foreach (Trigger j in ((TwinsSection)((TwinsSection)file.SecInfo.Records[i]).SecInfo.Records[7]).SecInfo.Records.Values)
+                        {
+                            GL.PushMatrix();
+                            GL.Translate(-j.Coords[1].X, j.Coords[1].Y, j.Coords[1].Z);
+
+                            GL.Begin(PrimitiveType.Quads);
+                            GL.Color4(colors[colors.Length - i - 1].R, colors[colors.Length - i - 1].G, colors[colors.Length - i - 1].B, (byte)128);
+
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+
+                            GL.End();
+
+                            GL.Disable(EnableCap.DepthTest);
+                            GL.LineWidth(2);
+                            GL.Begin(PrimitiveType.Lines);
+                            for (int k = 0; k < j.Instances.Length; ++k)
+                            {
+                                Instance inst = FileController.GetInstance(j.Parent.Parent.ID, j.Instances[k]);
+                                GL.Vertex3(0, 0, 0);
+                                GL.Vertex3(-inst.Pos.X + j.Coords[1].X, inst.Pos.Y - j.Coords[1].Y, inst.Pos.Z - j.Coords[1].Z);
+                            }
+                            GL.End();
+                            GL.LineWidth(1);
+
+                            GL.Color4(Color.Black);
+
+                            GL.Begin(PrimitiveType.LineLoop);
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.End();
+
+                            GL.Begin(PrimitiveType.LineLoop);
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+                            GL.End();
+
+                            GL.Begin(PrimitiveType.LineLoop);
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.End();
+
+                            GL.Begin(PrimitiveType.LineLoop);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.End();
+
+                            GL.Begin(PrimitiveType.LineLoop);
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, -j.Coords[2].Y, j.Coords[2].Z);
+                            GL.End();
+
+                            GL.Begin(PrimitiveType.LineLoop);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, -j.Coords[2].Z);
+                            GL.Vertex3(j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+                            GL.Vertex3(-j.Coords[2].X, j.Coords[2].Y, j.Coords[2].Z);
+                            GL.End();
+
+
+                            DrawAxes(0, 0, 0, Math.Min(j.Coords[2].X, Math.Min(j.Coords[2].Y, j.Coords[2].Z)) / 2);
+                            GL.Enable(EnableCap.DepthTest);
+
+                            GL.PopMatrix();
+                        }
+                    }
+                }
+            }
         }
 
         protected override bool IsInputKey(Keys keyData)
@@ -151,6 +254,7 @@ namespace TwinsaityEditor
             base.IsInputKey(keyData);
             switch (keyData)
             {
+                case Keys.C:
                 case Keys.T:
                     return true;
             }
@@ -162,10 +266,32 @@ namespace TwinsaityEditor
             base.OnKeyDown(e);
             switch (e.KeyCode)
             {
+                case Keys.C:
+                    show_col_nodes = !show_col_nodes;
+                    break;
                 case Keys.T:
-                    show_trigger = !show_trigger;
+                    show_triggers = !show_triggers;
                     break;
             }
+        }
+
+        private void DrawAxes(float x, float y, float z, float size)
+        {
+            GL.PushMatrix();
+            GL.Translate(x, y, z);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(1f, 0f, 0f);
+            float new_ind_size = indicator_size * size;
+            GL.Vertex3(+new_ind_size, 0, 0);
+            GL.Vertex3(-new_ind_size, 0, 0);
+            GL.Color3(0f, 1f, 0f);
+            GL.Vertex3(0, +new_ind_size, 0);
+            GL.Vertex3(0, -new_ind_size, 0);
+            GL.Color3(0f, 0f, 1f);
+            GL.Vertex3(0, 0, +new_ind_size);
+            GL.Vertex3(0, 0, -new_ind_size);
+            GL.End();
+            GL.PopMatrix();
         }
 
         /// <summary> 
