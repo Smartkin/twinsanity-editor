f190b6cfed0c7f98668bef10f38966078d3fad98
ManDude <mandude@users.noreply.github.com>
2018-09-19 22:50:15 +0100

    Add "Re-order" and "Re-ID" buttons to (object instance) sections

--- TwinsaityEditor/Controllers/SectionController.cs
+++ TwinsaityEditor/Controllers/SectionController.cs
@@ -1,4 +1,5 @@
 ﻿using Twinsanity;
+using System.Collections.Generic;
 
 namespace TwinsaityEditor
 {
@@ -10,6 +11,11 @@ namespace TwinsaityEditor
         {
             Toolbar = ToolbarFlags.Search | ToolbarFlags.Add | ToolbarFlags.Create | ToolbarFlags.Export;
             data = item;
+            if (item.Type == SectionType.ObjectInstance)
+            {
+                AddMenu("Re-order by ID (asc.)", Menu_ReOrderByID_Asc);
+                AddMenu("Re-ID by order", Menu_ReIDByOrder);
+            }
         }
 
         public override string GetName()
@@ -23,6 +29,38 @@ namespace TwinsaityEditor
             TextPrev[0] = "ID: " + data.ID;
             TextPrev[1] = "Offset: " + data.Offset + " Size: " + data.Size;
             TextPrev[2] = "ContentSize: " + data.ContentSize + " Element Count: " + data.SecInfo.Records.Count;
+        }
+
+        private void Menu_ReOrderByID_Asc()
+        {
+            Node.TreeView.BeginUpdate();
+            while (Node.Nodes.Count > 0)
+                DisposeNode(Node.Nodes[0]);
+            SortedDictionary<uint, TwinsItem> sdic = new SortedDictionary<uint, TwinsItem>(data.SecInfo.Records);
+            data.SecInfo.Records.Clear();
+            foreach (var i in sdic)
+            {
+                data.SecInfo.Records.Add(i.Key, i.Value);
+                ((MainForm)Node.TreeView.FindForm()).GenTreeNode(i.Value, this);
+            }
+            Node.TreeView.EndUpdate();
+        }
+
+        private void Menu_ReIDByOrder()
+        {
+            Node.TreeView.BeginUpdate();
+            while (Node.Nodes.Count > 0)
+                DisposeNode(Node.Nodes[0]);
+            Dictionary<uint, TwinsItem> dic = new Dictionary<uint, TwinsItem>(data.SecInfo.Records);
+            data.SecInfo.Records.Clear();
+            uint id = 0;
+            foreach(var i in dic)
+            {
+                i.Value.ID = id;
+                data.SecInfo.Records.Add(id++, i.Value);
+                ((MainForm)Node.TreeView.FindForm()).GenTreeNode(i.Value, this);
+            }
+            Node.TreeView.EndUpdate();
         }
     }
 }
