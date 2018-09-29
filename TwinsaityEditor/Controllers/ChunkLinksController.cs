a472dedf7a6b25ca12eab16bac1c0c4b6c278a25
ManDude <mandude@users.noreply.github.com>
2018-09-29 01:18:44 +0100

    Less spaces

--- TwinsaityEditor/Controllers/ChunkLinksController.cs
+++ TwinsaityEditor/Controllers/ChunkLinksController.cs
@@ -19,16 +19,16 @@ namespace TwinsaityEditor
 
         protected override void GenText()
         {
-            TextPrev = new string[3 + Data.Links.Count * 5];
+            TextPrev = new string[3 + Data.Links.Count * 4];
             TextPrev[0] = "ID: " + Data.ID;
             TextPrev[1] = "Offset: " + Data.Offset + " Size: " + Data.Size;
             TextPrev[2] = "LinkCount: " + Data.Links.Count;
             for (int i = 0; i < Data.Links.Count; ++i)
             {
-                TextPrev[4 + i * 5] = "Link" + i;
-                TextPrev[5 + i * 5] = "Type: " + Data.Links[i].Type;
-                TextPrev[6 + i * 5] = "Directory: " + new string(Data.Links[i].Path);
-                TextPrev[7 + i * 5] = "Flags: " + Convert.ToString(Data.Links[i].Flags, 16).ToUpper();
+                TextPrev[3 + i * 5] = "Link" + i;
+                TextPrev[4 + i * 5] = "Type: " + Data.Links[i].Type;
+                TextPrev[5 + i * 5] = "Directory: " + new string(Data.Links[i].Path);
+                TextPrev[6 + i * 5] = "Flags: " + Convert.ToString(Data.Links[i].Flags, 16).ToUpper();
             }
         }
     }
