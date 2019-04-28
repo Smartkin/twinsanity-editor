a8ea732ba77d464f3a69ecc8ebb35ffdbb91a377
ManDude <mandude@users.noreply.github.com>
2019-04-28 06:01:10 +0100

    Optimize some vector funcs

--- TwinsaityEditor/Utils/VectorFuncs.cs
+++ TwinsaityEditor/Utils/VectorFuncs.cs
@@ -7,18 +7,11 @@ namespace TwinsaityEditor
     {
         public static Vector3 CalcNormal(Vector3 Vert1, Vector3 Vert2, Vector3 Vert3)
         {
-            float x1 = Vert1.X;
-            float y1 = Vert1.Y;
-            float z1 = Vert1.Z;
-            float x2 = Vert2.X;
-            float y2 = Vert2.Y;
-            float z2 = Vert2.Z;
-            float x3 = Vert3.X;
-            float y3 = Vert3.Y;
-            float z3 = Vert3.Z;
-            float nx = ((y1 - y2) * (z1 - z3)) - ((z1 - z2) * (y1 - y3));
-            float ny = ((z1 - z2) * (x1 - x3)) - ((x1 - x2) * (z1 - z3));
-            float nz = ((x1 - x2) * (y1 - y3)) - ((y1 - y2) * (x1 - x3));
+            Vector3 u = Vert2 - Vert1;
+            Vector3 v = Vert3 - Vert1;
+            float nx = u.Y * v.Z - u.Z * v.Y;
+            float ny = u.Z * v.X - u.X * v.Z;
+            float nz = u.X * v.Y - u.Y * v.X;
             return new Vector3(nx, ny, nz);
         }
 
@@ -27,14 +20,14 @@ namespace TwinsaityEditor
             return new Vector4(pos.X, pos.Y, pos.Z, pos.W);
         }
 
-        public static Vector3 Pos2Vector3(Pos pos)
-        {
-            return new Vector3(pos.X, pos.Y, pos.Z);
-        }
-
         public static Pos ToPos(this Vector4 v)
         {
             return new Pos(v.X, v.Y, v.Z, v.W);
+        }
+
+        public static Vector3 ToVec3(this Pos p)
+        {
+            return new Vector3(p.X, p.Y, p.Z);
         }
 
         public static Vector4 NormalizeW(this Vector4 v)
