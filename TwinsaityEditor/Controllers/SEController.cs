6065dea1defde16a20174afe39c9a9e18b17c7c1
ManDude <mandude@users.noreply.github.com>
2019-05-23 22:33:22 +0100

    Add "Export to .WAV/.VAG" buttons

--- TwinsaityEditor/Controllers/SEController.cs
+++ TwinsaityEditor/Controllers/SEController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Media;
+using System.Windows.Forms;
 using Twinsanity;
 
 namespace TwinsaityEditor
@@ -19,6 +20,8 @@ namespace TwinsaityEditor
             Data = item;
             LoadSoundData();
             AddMenu("Play sound", Menu_PlaySound);
+            AddMenu("Export to .WAV", Menu_ExportWAV);
+            AddMenu("Export to .VAG", Menu_ExportVAG);
         }
 
         protected override string GetName()
@@ -43,8 +46,48 @@ namespace TwinsaityEditor
 
         private void Menu_PlaySound()
         {
+            player.Stop();
             player.Stream = new MemoryStream(SoundData);
             player.Play();
+        }
+
+        private void Menu_ExportWAV()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "WAV|*.wav";
+            sfd.FileName = Data.ID.ToString();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                FileStream file = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write);
+                BinaryWriter writer = new BinaryWriter(file);
+                writer.Write(SoundData);
+                writer.Close();
+            }
+        }
+
+        private void Menu_ExportVAG()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "VAG|*.vag";
+            var id_str = Data.ID.ToString();
+            sfd.FileName = id_str;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                FileStream file = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write);
+                BinaryWriter writer = new BinaryWriter(file);
+                writer.Write("VAGp".ToCharArray());
+                writer.Write(20);
+                writer.Write(0);
+                writer.Write(RawData.Length);
+                writer.Write(BitConv.FlipBytes(Data.Freq));
+                writer.Write(0); writer.Write(0); writer.Write(0);
+                char[] name = new char[16] { (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0 };
+                Array.Copy(id_str.ToCharArray(), name, Math.Min(id_str.Length, 16));
+                writer.Write(name);
+                //writer.Write(0); writer.Write(0); writer.Write(0); writer.Write(0);
+                writer.Write(RawData);
+                writer.Close();
+            }
         }
     }
 }
--- Twinsanity/BitConv.cs
+++ Twinsanity/BitConv.cs
@@ -7,5 +7,10 @@
             arr[off] = (byte)(value & 0xFF);
             arr[off + 1] = (byte)((value >> 8) & 0xFF);
         }
+
+        public static uint FlipBytes(uint val)
+        {
+            return (((val & 0x000000FFU) << 24) | ((val & 0x0000FF00U) << 8) | ((val & 0x00FF0000U) >> 8) | ((val & 0xFF000000U) >> 24));
+        }
     }
 }
