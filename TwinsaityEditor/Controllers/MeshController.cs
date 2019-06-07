f7ec3eadd59d09160c37c03060e4fbeac4b84566
ManDude <mandude@users.noreply.github.com>
2019-06-07 01:31:54 +0100

    MeshViewer: Move LoadMesh to MeshController + Fix normals

--- TwinsaityEditor/Controllers/MeshController.cs
+++ TwinsaityEditor/Controllers/MeshController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using OpenTK;
+using System.Collections.Generic;
 using System;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 using Twinsanity;
@@ -10,11 +12,15 @@ namespace TwinsaityEditor
     {
         public new Mesh Data { get; set; }
 
+        public Vertex[] Vertices { get; set; }
+        public uint[] Indices { get; set; }
+
         public MeshController(MainForm topform, Mesh item) : base (topform, item)
         {
             Data = item;
             AddMenu("Export to PLY", Menu_ExportPLY);
             AddMenu("Open mesh viewer", Menu_OpenViewer);
+            LoadMeshData();
         }
 
         protected override string GetName()
@@ -41,6 +47,71 @@ namespace TwinsaityEditor
             TextPrev[1] = $"Offset: {Data.Offset} Size: {Data.Size}";
             TextPrev[2] = $"SubMesh Count: {Data.SubModels.Count}";
             Array.Copy(ex_lines.ToArray(), 0, TextPrev, 3, ex_lines.Count);
+        }
+
+        private void LoadMeshData()
+        {
+            List<Vertex> vtx = new List<Vertex>();
+            List<uint> idx = new List<uint>();
+            int off = 0;
+            foreach (var s in Data.SubModels)
+            {
+                foreach (var g in s.Groups)
+                {
+                    if (g.VertHead > 0 && g.VDataHead > 0 && g.VertexCount >= 3)
+                    {
+                        vtx.Add(new Vertex(new Vector3(-g.Vertex[0].X, g.Vertex[0].Y, g.Vertex[0].Z), Color.FromArgb(g.VData[0].R, g.VData[0].G, g.VData[0].B)));
+                        vtx.Add(new Vertex(new Vector3(-g.Vertex[1].X, g.Vertex[1].Y, g.Vertex[1].Z), Color.FromArgb(g.VData[1].R, g.VData[1].G, g.VData[1].B)));
+                        for (int i = 2; i < g.VertexCount; ++i)
+                        {
+                            vtx.Add(new Vertex(new Vector3(-g.Vertex[i].X, g.Vertex[i].Y, g.Vertex[i].Z), Color.FromArgb(g.VData[i].R, g.VData[i].G, g.VData[i].B)));
+                            int v1 = off + i - 2 + (i & 1);
+                            int v2 = off + i - 1 - (i & 1);
+                            int v3 = off + i;
+                            if (g.VData[i].CONN == 128) continue;
+                            Vector3 normal = VectorFuncs.CalcNormal(vtx[v1].Pos, vtx[v2].Pos, vtx[v3].Pos);
+                            var v = vtx[v1];
+                            v.Nor += normal;
+                            vtx[v1] = v;
+                            v = vtx[v2];
+                            v.Nor += normal;
+                            vtx[v2] = v;
+                            v = vtx[v3];
+                            v.Nor += normal;
+                            vtx[v3] = v;
+                            idx.Add((uint)v1);
+                            idx.Add((uint)v2);
+                            idx.Add((uint)v3);
+                        }
+                        off += g.VertexCount;
+                    }
+                }
+            }
+            //sort out duplicates
+            for (int i = 0; i < vtx.Count; ++i)
+            {
+                Vector3 pos = vtx[i].Pos;
+                uint col = vtx[i].Col;
+                for (int j = i + 1; j < vtx.Count; ++j)
+                {
+                    if (vtx[j].Col == col && vtx[j].Pos == pos)
+                    {
+                        for (int k = 0; k < idx.Count; ++k)
+                        {
+                            if (idx[k] == j)
+                                idx[k] = (uint)i;
+                            else if (idx[k] > j)
+                                idx[k]--;
+                        }
+                        var v = vtx[i];
+                        v.Nor += vtx[j].Nor;
+                        vtx[i] = v;
+                        vtx.RemoveAt(j);
+                    }
+                }
+            }
+            Vertices = vtx.ToArray();
+            Indices = idx.ToArray();
         }
 
         private void Menu_ExportPLY()
--- TwinsaityEditor/Viewers/MeshViewer.cs
+++ TwinsaityEditor/Viewers/MeshViewer.cs
@@ -1,8 +1,5 @@
-﻿using OpenTK;
-using OpenTK.Graphics.OpenGL;
+﻿using OpenTK.Graphics.OpenGL;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace TwinsaityEditor
@@ -73,73 +70,17 @@ namespace TwinsaityEditor
         public void LoadMesh()
         {
             float min_x = float.MaxValue, min_y = float.MaxValue, min_z = float.MaxValue, max_x = float.MinValue, max_y = float.MinValue, max_z = float.MinValue;
-            List<Vertex> vtx_stack = new List<Vertex>();
-            List<uint> indx = new List<uint>();
-            int off = 0;
-            foreach (var s in mesh.Data.SubModels)
+            foreach (var v in mesh.Vertices)
             {
-                foreach (var g in s.Groups)
-                {
-                    if (g.VertHead > 0 && g.VDataHead > 0 && g.VertexCount >= 3)
-                    {
-                        vtx_stack.Add(new Vertex(new Vector3(-g.Vertex[0].X, g.Vertex[0].Y, g.Vertex[0].Z), Color.FromArgb(g.VData[0].R, g.VData[0].G, g.VData[0].B)));
-                        vtx_stack.Add(new Vertex(new Vector3(-g.Vertex[1].X, g.Vertex[1].Y, g.Vertex[1].Z), Color.FromArgb(g.VData[1].R, g.VData[1].G, g.VData[1].B)));
-                        for (int i = 2; i < g.VertexCount; ++i)
-                        {
-                            vtx_stack.Add(new Vertex(new Vector3(-g.Vertex[i].X, g.Vertex[i].Y, g.Vertex[i].Z), Color.FromArgb(g.VData[i].R, g.VData[i].G, g.VData[i].B)));
-                            int v1 = off + i - 2 + (i & 1);
-                            int v2 = off + i - 1 - (i & 1);
-                            int v3 = off + i;
-                            Vector3 normal = VectorFuncs.CalcNormal(vtx_stack[v1].Pos, vtx_stack[v2].Pos, vtx_stack[v3].Pos);
-                            var v = vtx_stack[v1];
-                            v.Nor += normal;
-                            vtx_stack[v1] = v;
-                            v = vtx_stack[v2];
-                            v.Nor += normal;
-                            vtx_stack[v2] = v;
-                            v = vtx_stack[v3];
-                            v.Nor += normal;
-                            vtx_stack[v3] = v;
-                            if (g.VData[i].CONN == 128) continue;
-                            indx.Add((uint)v1);
-                            indx.Add((uint)v2);
-                            indx.Add((uint)v3);
-                        }
-                        off += g.VertexCount;
-                    }
-                }
+                min_x = Math.Min(min_x, v.Pos.X);
+                min_y = Math.Min(min_y, v.Pos.Y);
+                min_z = Math.Min(min_z, v.Pos.Z);
+                max_x = Math.Max(max_x, v.Pos.X);
+                max_y = Math.Max(max_y, v.Pos.Y);
+                max_z = Math.Max(max_z, v.Pos.Z);
             }
-            //sort out duplicates
-            for (int i = 0; i < vtx_stack.Count; ++i)
-            {
-                Vector3 pos = vtx_stack[i].Pos;
-                uint col = vtx_stack[i].Col;
-                for (int j = i+1; j < vtx_stack.Count; ++j)
-                {
-                    if (vtx_stack[j].Col == col && vtx_stack[j].Pos == pos)
-                    {
-                        for (int k = 0; k < indx.Count; ++k)
-                        {
-                            if (indx[k] == j)
-                                indx[k] = (uint)i;
-                            else if (indx[k] > j)
-                                indx[k]--;
-                        }
-                        var v = vtx_stack[i];
-                        v.Nor += vtx_stack[j].Nor;
-                        vtx_stack[i] = v;
-                        vtx_stack.RemoveAt(j);
-                    }
-                }
-                min_x = Math.Min(min_x, vtx_stack[i].Pos.X);
-                min_y = Math.Min(min_y, vtx_stack[i].Pos.Y);
-                min_z = Math.Min(min_z, vtx_stack[i].Pos.Z);
-                max_x = Math.Max(max_x, vtx_stack[i].Pos.X);
-                max_y = Math.Max(max_y, vtx_stack[i].Pos.Y);
-                max_z = Math.Max(max_z, vtx_stack[i].Pos.Z);
-            }
-            vtx[0].Vtx = vtx_stack.ToArray();
-            vtx[0].VtxInd = indx.ToArray();
+            vtx[0].Vtx = mesh.Vertices;
+            vtx[0].VtxInd = mesh.Indices;
             zFar = Math.Max(zFar, Math.Max(max_x - min_x, Math.Max(max_y - min_y, max_z - min_z)));
             UpdateVBO(0);
         }
