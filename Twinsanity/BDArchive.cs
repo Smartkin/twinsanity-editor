4f3b113b79523a4e4adf7866227319ca2683913b
ManDude <mandude@users.noreply.github.com>
2019-12-23 23:34:21 +0000

    Rewrite BDArchive
    Currently only has static methods to extract a file to a folder and compile a folder to a file

--- Twinsanity/BDArchive.cs
+++ Twinsanity/BDArchive.cs
@@ -1,240 +1,154 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace Twinsanity
 {
     /// <summary>
     /// BD/BH archive class
     /// </summary>
-    public class BDArchive
+    public sealed class BDArchive
     {
-        /// <summary>
-        /// Simple structure of a file
-        /// </summary>
-        public struct File
+        public static void ExtractAll(string in_name, string out_dir)
         {
-            public int NameLength;
-            public string Name;
-            public uint Offset;
-            public uint Size;
-            public MemoryStream ByteStream;
-        }
-
-        private File[] TableRecord;
-        private int Records;
-
-        /// <summary>
-        /// Load the BH archive
-        /// </summary>
-        /// <param name="Path">Path to the archive</param>
-        /// <param name="Name">Name of the archive</param>
-        public void LoadTable(string Path, string Name)
-        {
-            Path = FormatPath(Path);
-            string archivePath = Path + Name + ".BH";
-
-            if (System.IO.File.Exists(archivePath))
+            string bdname = in_name, bhname = in_name;
+            if (in_name.EndsWith(".BH"))
             {
-                FileStream Table = new FileStream(archivePath, FileMode.Open, FileAccess.Read);
-                BinaryReader TableReader = new BinaryReader(Table);
-                Records = 0;
-                TableReader.ReadUInt32();
-                while (Table.Position < Table.Length)
+                bdname = in_name.Substring(0, in_name.Length-1) + "D";
+            }
+            else if (in_name.EndsWith(".bh"))
+            {
+                bdname = in_name.Substring(0, in_name.Length-1) + "d";
+            }
+            else if (in_name.EndsWith(".BD"))
+            {
+                bhname = in_name.Substring(0, in_name.Length-1) + "H";
+            }
+            else if (in_name.EndsWith(".bd"))
+            {
+                bhname = in_name.Substring(0, in_name.Length-1) + "h";
+            }
+            else
+            {
+                bdname += ".BD";
+                bhname += ".BH";
+            }
+            
+            if (!File.Exists(bhname))
+                throw new ArgumentException("BH file could not be found.");
+            if (!File.Exists(bdname))
+                throw new ArgumentException("BD file could not be found.");
+
+            using (BinaryReader hr = new BinaryReader(new FileStream(bhname, FileMode.Open)))
+            using (BinaryReader dr = new BinaryReader(new FileStream(bdname, FileMode.Open)))
+            {
+                for (int i = 0, s = hr.ReadInt32(); i < s; ++i)
                 {
-                    Array.Resize(ref TableRecord, Records + 1);
-                    int i = TableRecord.Length - 1;
-                    TableRecord[i].NameLength = (int)TableReader.ReadUInt32();
-                    TableRecord[i].Name = TableReader.ReadChars(TableRecord[i].NameLength).ToString();
-                    TableRecord[i].Offset = TableReader.ReadUInt32();
-                    TableRecord[i].Size = TableReader.ReadUInt32();
-                    Records += 1;
+                    int namelen = hr.ReadInt32();
+                    string name = Encoding.ASCII.GetString(hr.ReadBytes(namelen));
+                    uint offset = hr.ReadUInt32();
+                    uint size = hr.ReadUInt32();
+
+                    if (name.Contains(System.IO.Path.DirectorySeparatorChar.ToString()))
+                    {
+                        string dirs = name.Substring(0, name.LastIndexOf(System.IO.Path.DirectorySeparatorChar));
+
+                        Directory.CreateDirectory(System.IO.Path.Combine(out_dir, dirs));
+                    }
+
+                    using (BinaryWriter file = new BinaryWriter(new FileStream(System.IO.Path.Combine(out_dir, name), FileMode.Create, FileAccess.Write)))
+                    {
+                        dr.BaseStream.Position = offset;
+                        while (size > 0) // the files can be HUGE so we read/write in 2MB chunks
+                        {
+                            uint readsize = Math.Min(size, 2 * 1024 * 1024);
+                            file.Write(dr.ReadBytes((int)readsize));
+                            size -= readsize;
+                        }
+                    }
                 }
-                Table.Close();
-                Table.Dispose();
             }
         }
 
-        /// <summary>
-        /// Load the BD archive
-        /// </summary>
-        /// <param name="Path">Path to the archive</param>
-        /// <param name="Name">Name of the archive</param>
-        public void LoadArchive(string Path, string Name)
+        public static void CompileAll(string in_name, string out_dir)
         {
-            Path = FormatPath(Path);
-            string archivePath = Path + Name + ".BD";
-
-            if (System.IO.File.Exists(archivePath))
+            string bdname = in_name, bhname = in_name;
+            if (in_name.EndsWith(".BH"))
             {
-                FileStream Archive = new FileStream(archivePath, FileMode.Open, FileAccess.Read);
-                BinaryReader ArchiveReader = new BinaryReader(Archive);
-                BinaryWriter StreamWriter;
-                for (int i = 0; i <= Records - 1; i++)
+                bdname = in_name.Substring(0, in_name.Length - 1) + "D";
+            }
+            else if (in_name.EndsWith(".bh"))
+            {
+                bdname = in_name.Substring(0, in_name.Length - 1) + "d";
+            }
+            else if (in_name.EndsWith(".BD"))
+            {
+                bhname = in_name.Substring(0, in_name.Length - 1) + "H";
+            }
+            else if (in_name.EndsWith(".bd"))
+            {
+                bhname = in_name.Substring(0, in_name.Length - 1) + "h";
+            }
+            else
+            {
+                bdname += ".BD";
+                bhname += ".BH";
+            }
+
+            using (BinaryWriter hw = new BinaryWriter(new FileStream(bhname, FileMode.Create, FileAccess.Write)))
+            using (BinaryWriter dw = new BinaryWriter(new FileStream(bdname, FileMode.Create, FileAccess.Write)))
+            {
+                string filename = string.Empty;
+                DirectoryInfo di = new DirectoryInfo(out_dir);
+                Dictionary<BinaryReader,string> filemap = new Dictionary<BinaryReader,string>();
+
+                foreach (DirectoryInfo dir in di.GetDirectories())
                 {
-                    TableRecord[i].ByteStream = new MemoryStream();
-                    StreamWriter = new BinaryWriter(TableRecord[i].ByteStream);
-                    Archive.Position = TableRecord[i].Offset;
-                    StreamWriter.Write(ArchiveReader.ReadBytes((int)TableRecord[i].Size));
+                    LockDirectory(System.IO.Path.Combine(filename, dir.Name), dir, filemap);
                 }
-                Archive.Close();
-                Archive.Dispose();
-            }
-        }
-
-        /// <summary>
-        /// Create a table to save the archives at
-        /// </summary>
-        /// <param name="Path">Path of the table to be created at</param>
-        public void CreateTable(string Path)
-        {
-            Path = FormatPath(Path);
-            DirectoryInfo DirectoryInfo = new DirectoryInfo(Path);
-            string[] FileNames = Directory.GetFiles(Path, "*.*", SearchOption.AllDirectories);
-            Records = FileNames.Length;
-            FileStream File;
-            BinaryReader FileReader;
-            uint Size = 0;
-            Array.Resize(ref TableRecord, Records);
-            for (int i = 0; i <= Records - 1; i++)
-            {
-                File = new FileStream(FileNames[i], FileMode.Open, FileAccess.Read);
-                FileReader = new BinaryReader(File);
-                TableRecord[i].Offset = Size;
-                TableRecord[i].Size = (uint)File.Length;
-                Size += TableRecord[i].Size;
-                TableRecord[i].Name = FileNames[i].Replace(Path, "");
-                TableRecord[i].NameLength = TableRecord[i].Name.Length;
-                TableRecord[i].ByteStream = new MemoryStream();
-                BinaryWriter BSWriter = new BinaryWriter(TableRecord[i].ByteStream);
-                BSWriter.Write(FileReader.ReadBytes((int)File.Length));
-                File.Close();
-                File.Dispose();
-            }
-        }
-
-        /// <summary>
-        /// Save the BH archive
-        /// </summary>
-        /// <param name="Path">Path of saving</param>
-        /// <param name="Name">Name of the archive</param>
-        public void SaveTable(string Path, string Name)
-        {
-            Path = FormatPath(Path);
-            FileStream Table = new FileStream(Path + Name + ".BH", FileMode.Create, FileAccess.Write);
-            BinaryWriter TableWriter = new BinaryWriter(Table);
-            int Head = 1281;
-            TableWriter.Write(Head);
-            for (int i = 0; i <= Records - 1; i++)
-            {
-                TableWriter.Write(TableRecord[i].NameLength);
-                for (int j = 0; j <= TableRecord[i].NameLength - 1; j++)
-                    TableWriter.Write(TableRecord[i].Name[j]);
-                TableWriter.Write(TableRecord[i].Offset);
-                TableWriter.Write(TableRecord[i].Size);
-            }
-        }
-
-        /// <summary>
-        /// Save the BD archive
-        /// </summary>
-        /// <param name="Path">Path of saving</param>
-        /// <param name="Name">Name of the archive</param>
-        public void SaveArchive(string Path, string Name)
-        {
-            Path = FormatPath(Path);
-            FileStream Archive = new FileStream(Path + Name + ".BD", FileMode.Create, FileAccess.Write);
-            BinaryWriter ArchiveWriter = new BinaryWriter(Archive);
-            for (int i = 0; i <= Records - 1; i++)
-            {
-                Archive.Position = TableRecord[i].Offset;
-                ArchiveWriter.Write(TableRecord[i].ByteStream.ToArray());
-            }
-            Archive.Dispose();
-        }
-
-        /// <summary>
-        /// Extract the archives
-        /// </summary>
-        /// <param name="Path">Path to extract to</param>
-        public void Extract(string Path)
-        {
-            Path = FormatPath(Path);
-            FileStream File;
-            BinaryWriter FileWriter;
-            for (int i = 0; i <= Records - 1; i++)
-            {
-                string[] folders = TableRecord[i].Name.Split('\\');
-                string CheckPath = Path;
-                for (int j = 0; j <= folders.Length - 2; j++)
+                foreach (FileInfo file in di.GetFiles())
                 {
-                    CheckPath += folders[j] + @"\";
-                    if (!Directory.Exists(CheckPath))
-                        Directory.CreateDirectory(CheckPath);
+                    LockFile(System.IO.Path.Combine(filename, file.Name), file, filemap);
                 }
-                File = new FileStream(Path + TableRecord[i].Name, FileMode.Create, FileAccess.Write);
-                FileWriter = new BinaryWriter(File);
-                FileWriter.Write(TableRecord[i].ByteStream.ToArray());
-                File.Close();
-                File.Dispose();
-            }
-        }
 
-        /// <summary>
-        /// Realse the resources being used by the object
-        /// </summary>
-        public void Dispose()
-        {
-            for (int i = 0; i <= Records - 1; i++)
-                TableRecord[i].ByteStream.Dispose();
-            TableRecord = null;
-            Records = 0;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="Path1"></param>
-        /// <param name="Path2"></param>
-        /// <param name="Name"></param>
-        public void ExtractOnce(string Path1, string Path2, string Name)
-        {
-            Path1 = FormatPath(Path1);
-            Path2 = FormatPath(Path2);
-            LoadTable(Path2, Name);
-            FileStream File;
-            BinaryWriter FileWriter;
-            FileStream Archive = new FileStream(Path2 + Name + ".BD", FileMode.Open, FileAccess.Read);
-            BinaryReader ArchiveReader = new BinaryReader(Archive);
-            for (int i = 0; i <= Records - 1; i++)
-            {
-                string[] folders = TableRecord[i].Name.Split('\\');
-                string CheckPath = Path1;
-                for (int j = 0; j <= folders.Length - 2; j++)
+                hw.Write(filemap.Count);
+                foreach (var kvp in filemap)
                 {
-                    CheckPath += folders[j] + @"\";
-                    if (!Directory.Exists(CheckPath))
-                        Directory.CreateDirectory(CheckPath);
+                    if (kvp.Key.BaseStream.Length > uint.MaxValue)
+                        throw new IOException($"File {kvp.Value} is too large");
+                    byte[] name = Encoding.ASCII.GetBytes(kvp.Value);
+                    hw.Write(name.Length);
+                    hw.Write(name);
+                    hw.Write((uint)dw.BaseStream.Position);
+                    uint size = (uint)kvp.Key.BaseStream.Length;
+                    hw.Write(size);
+                    while (size > 0) // write in 2MB chunks at a time
+                    {
+                        uint readsize = Math.Min(size, 2 * 1024 * 1024);
+                        dw.Write(kvp.Key.ReadBytes((int)readsize));
+                        size -= readsize;
+                    }
+                    kvp.Key.Close();
                 }
-                File = new FileStream(Path1 + TableRecord[i].Name, FileMode.Create, FileAccess.Write);
-                FileWriter = new BinaryWriter(File);
-                Archive.Position = TableRecord[i].Offset;
-                FileWriter.Write(ArchiveReader.ReadBytes((int)TableRecord[i].Size));
-                File.Close();
-                File.Dispose();
             }
-            Archive.Dispose();
         }
 
-        /// <summary>
-        /// Formats the path to normalize it
-        /// </summary>
-        /// <param name="Path">Path to format</param>
-        /// <returns>Formatted path suitable for usage</returns>
-        public string FormatPath(string Path)
+        internal static void LockDirectory(string dname, DirectoryInfo di, Dictionary<BinaryReader,string> filemap)
         {
-            if (!(Path[Path.Length - 1].Equals('\\')))
-                return Path + @"\";
-            return Path;
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                LockDirectory(System.IO.Path.Combine(dname, dir.Name), dir, filemap);
+            }
+            foreach (FileInfo file in di.GetFiles())
+            {
+                LockFile(System.IO.Path.Combine(dname, file.Name), file, filemap);
+            }
+        }
+
+        internal static void LockFile(string filename, FileInfo file, Dictionary<BinaryReader,string> filemap)
+        {
+            filemap.Add(new BinaryReader(file.Open(FileMode.Open, FileAccess.Read)), filename);
         }
     }
 }
