5d80c2a7a53c314bb10b2f93e45193e482cdae38
Neo_Kesha <neokesha@yandex.ru>
2020-08-16 02:15:14 +0300

    Script Editor navigation

--- TwinsaityEditor/Editors/ScriptEditor.Designer.cs
+++ TwinsaityEditor/Editors/ScriptEditor.Designer.cs
@@ -28,12 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.scriptTree = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelType1 = new System.Windows.Forms.Panel();
+            this.panelType2 = new System.Windows.Forms.Panel();
+            this.panelType3 = new System.Windows.Forms.Panel();
+            this.panelType4 = new System.Windows.Forms.Panel();
+            this.panelLinked = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(213, 553);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // scriptTree
+            // 
+            this.scriptTree.Dock = System.Windows.Forms.DockStyle.Left;
+            this.scriptTree.Location = new System.Drawing.Point(213, 0);
+            this.scriptTree.Name = "scriptTree";
+            this.scriptTree.Size = new System.Drawing.Size(214, 553);
+            this.scriptTree.TabIndex = 1;
+            this.scriptTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.scriptTree_AfterSelect);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panelHeader);
+            this.groupBox1.Controls.Add(this.panelMain);
+            this.groupBox1.Controls.Add(this.panelType1);
+            this.groupBox1.Controls.Add(this.panelType2);
+            this.groupBox1.Controls.Add(this.panelType3);
+            this.groupBox1.Controls.Add(this.panelType4);
+            this.groupBox1.Controls.Add(this.panelLinked);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(427, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 553);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Parameters";
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelHeader.Location = new System.Drawing.Point(3, 16);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(303, 534);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // panelMain
+            // 
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(3, 16);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(303, 534);
+            this.panelMain.TabIndex = 0;
+            // 
+            // panelType1
+            // 
+            this.panelType1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelType1.Location = new System.Drawing.Point(3, 16);
+            this.panelType1.Name = "panelType1";
+            this.panelType1.Size = new System.Drawing.Size(303, 534);
+            this.panelType1.TabIndex = 0;
+            // 
+            // panelType2
+            // 
+            this.panelType2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelType2.Location = new System.Drawing.Point(3, 16);
+            this.panelType2.Name = "panelType2";
+            this.panelType2.Size = new System.Drawing.Size(303, 534);
+            this.panelType2.TabIndex = 0;
+            // 
+            // panelType3
+            // 
+            this.panelType3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelType3.Location = new System.Drawing.Point(3, 16);
+            this.panelType3.Name = "panelType3";
+            this.panelType3.Size = new System.Drawing.Size(303, 534);
+            this.panelType3.TabIndex = 0;
+            // 
+            // panelType4
+            // 
+            this.panelType4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelType4.Location = new System.Drawing.Point(3, 16);
+            this.panelType4.Name = "panelType4";
+            this.panelType4.Size = new System.Drawing.Size(303, 534);
+            this.panelType4.TabIndex = 0;
+            // 
+            // panelLinked
+            // 
+            this.panelLinked.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelLinked.Location = new System.Drawing.Point(3, 16);
+            this.panelLinked.Name = "panelLinked";
+            this.panelLinked.Size = new System.Drawing.Size(303, 534);
+            this.panelLinked.TabIndex = 0;
+            // 
+            // ScriptEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(736, 553);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.scriptTree);
+            this.Controls.Add(this.listBox1);
+            this.Name = "ScriptEditor";
             this.Text = "ScriptEditor";
+            this.Load += new System.EventHandler(this.ScriptEditor_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TreeView scriptTree;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Panel panelType1;
+        private System.Windows.Forms.Panel panelType2;
+        private System.Windows.Forms.Panel panelType3;
+        private System.Windows.Forms.Panel panelType4;
+        private System.Windows.Forms.Panel panelLinked;
     }
 }
--- TwinsaityEditor/Editors/ScriptEditor.cs
+++ TwinsaityEditor/Editors/ScriptEditor.cs
@@ -7,14 +7,168 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Twinsanity;
 
 namespace TwinsaityEditor
 {
     public partial class ScriptEditor : Form
     {
-        public ScriptEditor(SectionController cont)
+        private SectionController controller;
+        private Script script;
+
+        private FileController File { get; set; }
+        private TwinsFile FileData { get => File.Data; }
+        public ScriptEditor(SectionController c)
         {
+            File = c.MainFile;
+            controller = c;
+            Text = $"Instance Editor (Section {c.Data.Parent.ID})";
             InitializeComponent();
+            PopulateList();
+            UpdatePanels();
+        }
+
+        private void ScriptEditor_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void PopulateList()
+        {
+            listBox1.BeginUpdate();
+            listBox1.Items.Clear();
+            foreach (Script i in controller.Data.Records)
+            {
+                listBox1.Items.Add(GenTextForList(i));
+            }
+            listBox1.EndUpdate();
+        }
+        private string GenTextForList(Script script)
+        {
+            return $"ID {script.ID} {(script.Name == string.Empty ? string.Empty : $" - {script.Name}")}";
+        }
+        private void BuildTree()
+        {
+            scriptTree.BeginUpdate();
+            scriptTree.Nodes.Clear();
+            scriptTree.Nodes.Add(script.Name);
+            if (script.HeaderScript != null)
+            {
+                scriptTree.TopNode.Nodes.Add("Header Script").Tag = script.HeaderScript;
+            } 
+            if (script.MainScript != null)
+            {
+                TreeNode mainScriptNode = scriptTree.TopNode.Nodes.Add("Main Script");
+                mainScriptNode.Tag = script.MainScript;
+                Script.MainScriptStruct mainScript = script.MainScript;
+                Script.MainScriptStruct.LinkedScript ptr = mainScript.linkedScript1;
+                while (ptr != null)
+                {
+                    AddLinked(mainScriptNode, ptr);
+                    ptr = ptr.nextLinked;
+                }
+            }
+            scriptTree.EndUpdate();
+        }
+        private void AddLinked(TreeNode parent, Script.MainScriptStruct.LinkedScript ptr)
+        {
+            TreeNode node = parent.Nodes.Add($"Linked Script {ptr.scriptIndexOrSlot}");
+            node.Tag = ptr;
+            AddType1(node, ptr.type1);
+            Script.MainScriptStruct.SupportType2 ptrType2 = ptr.type2;
+            while (ptrType2 != null)
+            {
+                AddType2(node, ptrType2);
+                ptrType2 = ptrType2.nextType2;
+            }
+        }
+        private void AddType1(TreeNode parent, Script.MainScriptStruct.SupportType1 ptr)
+        {
+            TreeNode node = parent.Nodes.Add($"Type 1");
+            node.Tag = ptr;
+        }
+        private void AddType2(TreeNode parent, Script.MainScriptStruct.SupportType2 ptr)
+        {
+            TreeNode node = parent.Nodes.Add($"Type 2 Linked Slot: {ptr.linkedScriptListIndex}");
+            node.Tag = ptr;
+            AddType3(node, ptr.type3);
+            Script.MainScriptStruct.SupportType4 ptrType4 = ptr.type4;
+            while (ptrType4 != null)
+            {
+                AddType4(node, ptrType4);
+                ptrType4 = ptrType4.nextType4;
+            }
+        }
+        private void AddType3(TreeNode parent, Script.MainScriptStruct.SupportType3 ptr)
+        {
+            TreeNode node = parent.Nodes.Add($"Type 3");
+            node.Tag = ptr;
+        }
+        private void AddType4(TreeNode parent, Script.MainScriptStruct.SupportType4 ptr)
+        {
+            TreeNode node = parent.Nodes.Add($"Type 4 ");
+            node.Tag = ptr;
+        }
+        private void UpdatePanels()
+        {
+            panelHeader.Visible = false;
+            panelMain.Visible = false;
+            panelType1.Visible = false;
+            panelType2.Visible = false;
+            panelType3.Visible = false;
+            panelType4.Visible = false;
+            panelLinked.Visible = false;
+            if (null != scriptTree.SelectedNode)
+            {
+                Object tag = scriptTree.SelectedNode.Tag;
+                if (tag is Script.HeaderScriptStruct)
+                {
+                    panelHeader.Visible = true;
+                }
+                if (tag is Script.MainScriptStruct)
+                {
+                    panelMain.Visible = true;
+                }
+                if (tag is Script.MainScriptStruct.SupportType1)
+                {
+                    panelType1.Visible = true;
+                }
+                if (tag is Script.MainScriptStruct.SupportType2)
+                {
+                    panelType2.Visible = true;
+                }
+                if (tag is Script.MainScriptStruct.SupportType3)
+                {
+                    panelType3.Visible = true;
+                }
+                if (tag is Script.MainScriptStruct.SupportType4)
+                {
+                    panelType4.Visible = true;
+                }
+                if (tag is Script.MainScriptStruct.LinkedScript)
+                {
+                    panelLinked.Visible = true;
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (null != listBox1.SelectedItem)
+            {
+                File.SelectItem((Script)controller.Data.Records[listBox1.SelectedIndex]);
+                script = (Script)File.SelectedItem;
+            } 
+            else
+            {
+                File.SelectItem(null);
+                script = null;
+            }
+            BuildTree();
+        }
+
+        private void scriptTree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            UpdatePanels();
         }
     }
 }
