9c30062b89269b8022a64543959acd5bd7c14a38
Neo_Kesha <neokesha@yandex.ru>
2020-08-17 06:44:15 +0300

    Slot sync

--- TwinsaityEditor/Utils/DataManipulators.cs
+++ TwinsaityEditor/Utils/DataManipulators.cs
@@ -20,12 +20,12 @@ namespace TwinsaityEditor.Utils
             
             this.source = source;
             this.listBox = listBox;
-            listBox.SelectedIndexChanged += UpdateSource;
-            addBtn.Click += Add;
-            delBtn.Click += Remove;
-            setBtn.Click += Set;
-            upBtn.Click += MoveUp;
-            downBtn.Click += MoveDown;
+            if (listBox != null) listBox.SelectedIndexChanged += UpdateSource;
+            if (addBtn != null) addBtn.Click += Add;
+            if (delBtn != null) delBtn.Click += Remove;
+            if (setBtn != null) setBtn.Click += Set;
+            if (upBtn != null) upBtn.Click += MoveUp;
+            if (downBtn != null) downBtn.Click += MoveDown;
             
         }
         public void SetSource(List<UInt16> list)
@@ -159,12 +159,12 @@ namespace TwinsaityEditor.Utils
 
             this.source = source;
             this.listBox = listBox;
-            listBox.SelectedIndexChanged += UpdateSource;
-            addBtn.Click += Add;
-            delBtn.Click += Remove;
-            setBtn.Click += Set;
-            upBtn.Click += MoveUp;
-            downBtn.Click += MoveDown;
+            if (listBox != null) listBox.SelectedIndexChanged += UpdateSource;
+            if (addBtn != null) addBtn.Click += Add;
+            if (delBtn != null) delBtn.Click += Remove;
+            if (setBtn != null) setBtn.Click += Set;
+            if (upBtn != null) upBtn.Click += MoveUp;
+            if (downBtn != null) downBtn.Click += MoveDown;
 
         }
         public void SetSource(List<UInt32> list)
@@ -297,12 +297,12 @@ namespace TwinsaityEditor.Utils
 
             this.source = source;
             this.listBox = listBox;
-            listBox.SelectedIndexChanged += UpdateSource;
-            addBtn.Click += Add;
-            delBtn.Click += Remove;
-            setBtn.Click += Set;
-            upBtn.Click += MoveUp;
-            downBtn.Click += MoveDown;
+            if (listBox != null) listBox.SelectedIndexChanged += UpdateSource;
+            if (addBtn != null) addBtn.Click += Add;
+            if (delBtn != null) delBtn.Click += Remove;
+            if (setBtn != null) setBtn.Click += Set;
+            if (upBtn != null) upBtn.Click += MoveUp;
+            if (downBtn != null) downBtn.Click += MoveDown;
 
         }
         public void SetSource(List<Single> list)
@@ -424,4 +424,143 @@ namespace TwinsaityEditor.Utils
             update = true;
         }
     }
+    public class ListManipulatorByte
+    {
+        List<Byte> list;
+        ListBox listBox;
+        TextBox source;
+        bool update;
+        public ListManipulatorByte(Button addBtn, Button delBtn, Button setBtn, Button upBtn, Button downBtn, ListBox listBox, TextBox source)
+        {
+
+            this.source = source;
+            this.listBox = listBox;
+            if (listBox != null) listBox.SelectedIndexChanged += UpdateSource;
+            if (addBtn != null) addBtn.Click += Add;
+            if (delBtn != null) delBtn.Click += Remove;
+            if (setBtn != null) setBtn.Click += Set;
+            if (upBtn != null) upBtn.Click += MoveUp;
+            if (downBtn != null) downBtn.Click += MoveDown;
+
+        }
+        public void SetSource(List<Byte> list)
+        {
+            this.list = list;
+            update = true;
+        }
+        public void UpdateSource(Object sender, EventArgs args)
+        {
+            if (update)
+            {
+                source.Text = list[listBox.SelectedIndex].ToString();
+            }
+        }
+        public void PopulateList()
+        {
+            listBox.BeginUpdate();
+            listBox.Items.Clear();
+            foreach (Single e in list)
+            {
+                listBox.Items.Add(e.ToString());
+            }
+            listBox.EndUpdate();
+        }
+
+        public void MoveUp(Object sender, EventArgs args)
+        {
+            if (listBox.SelectedIndex > 0)
+            {
+                int index = listBox.SelectedIndex;
+                Byte val1 = list[index];
+                Byte val2 = list[index - 1];
+                list[index - 1] = val1;
+                list[index] = val2;
+                DisableUpdate();
+                listBox.Items[index] = list[index].ToString();
+                listBox.Items[index - 1] = list[index - 1].ToString();
+                int top = listBox.Items.Count - 1;
+                --index;
+                listBox.SelectedIndex = Math.Min(Math.Max(index, 0), top);
+                EnableUpdate();
+            }
+        }
+        public void MoveDown(Object sender, EventArgs args)
+        {
+            if (listBox.SelectedIndex < listBox.Items.Count - 1)
+            {
+                int index = listBox.SelectedIndex;
+                Byte val1 = list[index];
+                Byte val2 = list[index + 1];
+                list[index + 1] = val1;
+                list[index] = val2;
+                DisableUpdate();
+                listBox.Items[index] = list[index].ToString();
+                listBox.Items[index + 1] = list[index + 1].ToString();
+                int top = listBox.Items.Count - 1;
+                ++index;
+                listBox.SelectedIndex = Math.Min(Math.Max(index, 0), top);
+                EnableUpdate();
+            }
+        }
+        public void Remove(Object sender, EventArgs args)
+        {
+            if (listBox.SelectedIndex >= 0)
+            {
+                int index = listBox.SelectedIndex;
+                list.RemoveAt(index);
+                DisableUpdate();
+                listBox.Items.RemoveAt(index);
+                int top = listBox.Items.Count - 1;
+                listBox.SelectedIndex = Math.Min(Math.Max(index, 0), top);
+                EnableUpdate();
+            }
+        }
+        public void Add(Object sender, EventArgs args)
+        {
+            int index = listBox.SelectedIndex;
+            if (index < 0)
+            {
+                index = 0;
+            }
+            Byte val;
+            Boolean success;
+            success = Byte.TryParse(source.Text, out val);
+            if (success)
+            {
+                list.Insert(index, val);
+                DisableUpdate();
+                listBox.Items.Insert(index, source.Text);
+                listBox.SelectedIndex = index;
+                EnableUpdate();
+            }
+
+        }
+        public void Set(Object sender, EventArgs args)
+        {
+            if (listBox.SelectedIndex >= 0)
+            {
+                Byte val;
+                Boolean success;
+                success = Byte.TryParse(source.Text, out val);
+                if (success)
+                {
+                    list[listBox.SelectedIndex] = val;
+                    int index = listBox.SelectedIndex;
+                    DisableUpdate();
+                    listBox.Items[listBox.SelectedIndex] = source.Text;
+                    listBox.SelectedIndex = index;
+                    EnableUpdate();
+                }
+            }
+        }
+        private void DisableUpdate()
+        {
+            update = false;
+        }
+        private void EnableUpdate()
+        {
+            update = true;
+        }
+    }
+
 }
