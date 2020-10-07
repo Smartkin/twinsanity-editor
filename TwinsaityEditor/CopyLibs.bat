dae1d067f02404aedf6228b436f6a91cc5ff737c
Smartkin <vladislav5019@gmail.com>
2020-10-07 18:39:11 +0300

    Update batch script to copy Fonts

--- TwinsaityEditor/CopyLibs.bat
+++ TwinsaityEditor/CopyLibs.bat
@@ -2,11 +2,13 @@
 MKDIR Libraries
 MKDIR x64
 MKDIR x86
+MKDIR Fonts
+COPY /Y %1\Fonts\* Fonts
 COPY /Y ..\..\packages\SharpFont.Dependencies.2.6\bin\msvc12\x64 x64
 COPY /Y ..\..\packages\SharpFont.Dependencies.2.6\bin\msvc12\x86 x86
 MOVE /Y *.dll Libraries
-COPY /Y %1\Libx64\*.dll x64
-COPY /Y %1\Libx86\*.dll x86
+COPY /Y %1\Externals\Libx64\*.dll x64
+COPY /Y %1\Externals\Libx86\*.dll x86
 echo "Renaming 64-bit libraries"
 SETLOCAL enabledelayedexpansion
 SET FOLDER_PATH="x64\*64.dll"
