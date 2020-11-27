1f11ebb883582080fcf96e8a3d1626b260624571
ManDude <7569514+ManDude@users.noreply.github.com>
2020-11-27 05:14:42 +0000

    BDExplorer: Improve UI

--- TwinsaityEditor/Workers/BDExplorer.Designer.cs
+++ TwinsaityEditor/Workers/BDExplorer.Designer.cs
@@ -36,7 +36,9 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // archiveContentsTree
@@ -44,14 +46,14 @@
             this.archiveContentsTree.Dock = System.Windows.Forms.DockStyle.Fill;
             this.archiveContentsTree.Location = new System.Drawing.Point(0, 0);
             this.archiveContentsTree.Name = "archiveContentsTree";
-            this.archiveContentsTree.Size = new System.Drawing.Size(550, 534);
+            this.archiveContentsTree.Size = new System.Drawing.Size(329, 468);
             this.archiveContentsTree.TabIndex = 0;
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusBar});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 512);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 468);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(550, 22);
             this.statusStrip1.TabIndex = 2;
@@ -67,8 +69,8 @@
             // 
             this.OpenBDBH.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.OpenBDBH.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.OpenBDBH.Font = new System.Drawing.Font("Twelve Ton Goldfish", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.OpenBDBH.Location = new System.Drawing.Point(321, 12);
+            this.OpenBDBH.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpenBDBH.Location = new System.Drawing.Point(10, 12);
             this.OpenBDBH.Name = "OpenBDBH";
             this.OpenBDBH.Size = new System.Drawing.Size(199, 85);
             this.OpenBDBH.TabIndex = 3;
@@ -80,8 +82,8 @@
             // 
             this.SaveButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.SaveButton.Font = new System.Drawing.Font("Twelve Ton Goldfish", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SaveButton.Location = new System.Drawing.Point(321, 113);
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.Location = new System.Drawing.Point(10, 103);
             this.SaveButton.Name = "SaveButton";
             this.SaveButton.Size = new System.Drawing.Size(199, 85);
             this.SaveButton.TabIndex = 4;
@@ -93,8 +95,8 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Twelve Ton Goldfish", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(321, 214);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(10, 194);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(199, 85);
             this.button2.TabIndex = 5;
@@ -106,8 +108,8 @@
             // 
             this.button3.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Font = new System.Drawing.Font("Twelve Ton Goldfish", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(321, 314);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(10, 285);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(199, 85);
             this.button3.TabIndex = 6;
@@ -119,8 +121,8 @@
             // 
             this.button4.BackColor = System.Drawing.Color.Firebrick;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button4.Font = new System.Drawing.Font("Twelve Ton Goldfish", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(321, 415);
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(10, 376);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(199, 85);
             this.button4.TabIndex = 7;
@@ -128,25 +130,34 @@
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.OpenBDBH);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.SaveButton);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(329, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(221, 468);
+            this.panel1.TabIndex = 8;
+            // 
             // BDExplorer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(550, 534);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.SaveButton);
-            this.Controls.Add(this.OpenBDBH);
-            this.Controls.Add(this.statusStrip1);
+            this.ClientSize = new System.Drawing.Size(550, 490);
             this.Controls.Add(this.archiveContentsTree);
-            this.MaximumSize = new System.Drawing.Size(566, 99999);
-            this.MinimumSize = new System.Drawing.Size(566, 573);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.MinimumSize = new System.Drawing.Size(566, 529);
             this.Name = "BDExplorer";
             this.Text = "BDExplorer";
             this.Load += new System.EventHandler(this.BDExplorer_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,5 +173,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
     }
 }
