fdaf19f430943df5e741e62452e405bbc54d86c0
BetaM <foreverbetam@gmail.com>
2021-02-13 22:15:11 +0100

    Update project to .NET 4.7

--- TwinsaityEditor/Properties/Settings.Designer.cs
+++ TwinsaityEditor/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace TwinsaityEditor.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -142,17 +142,15 @@ namespace TwinsaityEditor.Properties {
                 this["ImageOutputPath"] = value;
             }
         }
+        
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string PCSX2Path
-        {
-            get
-            {
+        public string PCSX2Path {
+            get {
                 return ((string)(this["PCSX2Path"]));
             }
-            set
-            {
+            set {
                 this["PCSX2Path"] = value;
             }
         }
